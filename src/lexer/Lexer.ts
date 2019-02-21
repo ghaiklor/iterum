04ad53357d851fe74ff936a5096a350b9dd7dccf
Eugene Obrezkov <ghaiklor@gmail.com>
2019-02-21 15:47:53 +0200

    Implement lexer that supports all the tokens

--- src/lexer/Lexer.ts
+++ src/lexer/Lexer.ts
@@ -57,24 +57,94 @@ export class Lexer {
       this.skipWhitespace();
     }
 
-    if (this.currentChar.is("*")) {
+    if (this.currentChar.isDigit()) {
+      let buffer: string = "";
+
+      while (this.currentChar.isDigit()) {
+        buffer += this.currentChar;
+        this.advance();
+      }
+
+      return this.createToken(TokenType.NUMBER_LITERAL, buffer);
+    } else if (this.currentChar.isAlpha()) {
+      let buffer: string = "";
+
+      while (this.currentChar.isAlphaNumeric()) {
+        buffer += this.currentChar;
+        this.advance();
+      }
+
+      if (buffer === "function") {
+        return this.createToken(TokenType.FUNCTION, "function");
+      }
+
+      if (buffer === "return") {
+        return this.createToken(TokenType.RETURN, "return");
+      }
+
+      return this.createToken(TokenType.IDENTIFIER, buffer);
+    } else if (this.currentChar.is('"')) {
+      let buffer: string = "";
       this.advance();
-      return new Token(TokenType.ASTERISK, "*", this.cursorLocation);
+
+      while (!this.currentChar.is('"')) {
+        buffer += this.currentChar;
+        this.advance();
+      }
+
+      return this.createToken(TokenType.STRING_LITERAL, buffer);
+    } else if (this.currentChar.is("'")) {
+      let buffer: string = "";
+      this.advance();
+
+      while (!this.currentChar.is("'")) {
+        buffer += this.currentChar;
+        this.advance();
+      }
+
+      return this.createToken(TokenType.STRING_LITERAL, buffer);
+    } else if (this.currentChar.is("*")) {
+      this.advance();
+      return this.createToken(TokenType.ASTERISK, "*");
     } else if (this.currentChar.is("+")) {
       this.advance();
-      return new Token(TokenType.PLUS, "+", this.cursorLocation);
+      return this.createToken(TokenType.PLUS, "+");
     } else if (this.currentChar.is("-")) {
       this.advance();
-      return new Token(TokenType.MINUS, "-", this.cursorLocation);
+      return this.createToken(TokenType.MINUS, "-");
     } else if (this.currentChar.is("/")) {
       this.advance();
-      return new Token(TokenType.SLASH, "/", this.cursorLocation);
+      return this.createToken(TokenType.SLASH, "/");
     } else if (this.currentChar.is("\\")) {
       this.advance();
-      return new Token(TokenType.BACKSLASH, "\\", this.cursorLocation);
+      return this.createToken(TokenType.BACKSLASH, "\\");
+    } else if (this.currentChar.is("=")) {
+      this.advance();
+      return this.createToken(TokenType.ASSIGN, "=");
+    } else if (this.currentChar.is(",")) {
+      this.advance();
+      return this.createToken(TokenType.COMMA, ",");
+    } else if (this.currentChar.is("(")) {
+      this.advance();
+      return this.createToken(TokenType.LEFT_PARENTHESIS, "(");
+    } else if (this.currentChar.is(")")) {
+      this.advance();
+      return this.createToken(TokenType.RIGHT_PARENTHESIS, ")");
+    } else if (this.currentChar.is("{")) {
+      this.advance();
+      return this.createToken(TokenType.LEFT_CURLY_BRACES, "{");
+    } else if (this.currentChar.is("}")) {
+      this.advance();
+      return this.createToken(TokenType.RIGHT_CURLY_BRACES, "}");
+    } else if (this.currentChar.is(":")) {
+      this.advance();
+      return this.createToken(TokenType.COLON, ":");
+    } else if (this.currentChar.is(";")) {
+      this.advance();
+      return this.createToken(TokenType.SEMICOLON, ";");
     }
 
-    throw new Error(`Unrecognized token at ${this.cursorLocation.line}:${this.cursorLocation.column}`);
+    throw new Error(`Unrecognized character ${this.currentChar} at ${this.cursorLocation}`);
   }
 
   private skipWhitespace() {
