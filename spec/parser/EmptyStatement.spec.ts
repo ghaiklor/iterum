e47134e69c4c699ad7c5ce480c9766b70032a551
Eugene Obrezkov <ghaiklor@gmail.com>
2019-04-05 11:26:13 +0300

    test: 💍 empty statement

---
+++ spec/parser/EmptyStatement.spec.ts
@@ -0,0 +1,19 @@
+import { IProgram } from "../../src/ast/programs/Program";
+import { IEmptyStatement } from "../../src/ast/statements/EmptyStatement";
+import { Parser } from "../../src/parser/Parser";
+
+describe("Iterum::Parser::EmptyStatement", () => {
+  it("Should properly parse the statement", () => {
+    const source = `;`;
+    const ast = Parser.parse(source);
+
+    expect(ast).toMatchObject({
+      body: [{
+        loc: null,
+        type: "EmptyStatement",
+      } as IEmptyStatement],
+      loc: null,
+      type: "Program",
+    } as IProgram);
+  });
+});
