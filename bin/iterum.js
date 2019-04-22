938f6181863c5514e2978105a8cda6cf3f110068
Eugene Obrezkov <ghaiklor@gmail.com>
2019-04-22 15:49:01 +0300

    feat: 🎸 add a simple CLI over iterum to print AST at least

---
+++ bin/iterum.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const fs = require('fs');
+const path = require('path');
+const version = require('../package.json').version;
+const parse = require('../dist/parser/Parser').Parser.parse;
+
+program
+  .version(version, '--version')
+  .usage('[options] <file...>')
+  .option('--print-ast', 'Print the AST after parsing the source language')
+  .parse(process.argv);
+
+const file = program.args[0];
+if (!file || typeof file !== 'string' || !fs.existsSync(file)) {
+  console.log(`File ${file} does not exists`);
+  process.exit(1);
+}
+
+const source = fs.readFileSync(file, 'utf-8');
+if (program.printAst) {
+  console.log(JSON.stringify(parse(source), null, 2));
+}
