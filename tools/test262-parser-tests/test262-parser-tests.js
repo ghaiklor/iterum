63044eabdec6b09f1f6286da25968bc3f841f781
Eugene Obrezkov <ghaiklor@gmail.com>
2019-05-15 21:17:41 +0300

    style: 💄 fix eslint issues with test262 runner

--- tools/test262-parser-tests/test262-parser-tests.js
+++ tools/test262-parser-tests/test262-parser-tests.js
@@ -1,21 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-const assert = require('assert');
-const chalk = require('chalk');
-const parse = require('../../dist/parser/Parser').Parser.parse;
+const fs = require("fs");
+const path = require("path");
+const assert = require("assert");
+const chalk = require("chalk");
+const parse = require("../../dist/parser/Parser").Parser.parse;
 
 const EXCLUDES = {
-  'pass': require('./pass.json'),
-  'pass-explicit': require('./pass-explicit.json'),
-  'early': require('./early.json'),
-  'fail': require('./fail.json')
+  "pass": require("./pass.json"),
+  "pass-explicit": require("./pass-explicit.json"),
+  "early": require("./early.json"),
+  "fail": require("./fail.json")
 };
 
 const TESTS = {
-  'pass': path.resolve(__dirname, '../../node_modules/test262-parser-tests/pass'),
-  'pass-explicit': path.resolve(__dirname, '../../node_modules/test262-parser-tests/pass-explicit'),
-  'early': path.resolve(__dirname, '../../node_modules/test262-parser-tests/early'),
-  'fail': path.resolve(__dirname, '../../node_modules/test262-parser-tests/fail'),
+  "pass": path.resolve(__dirname, "../../node_modules/test262-parser-tests/pass"),
+  "pass-explicit": path.resolve(__dirname, "../../node_modules/test262-parser-tests/pass-explicit"),
+  "early": path.resolve(__dirname, "../../node_modules/test262-parser-tests/early"),
+  "fail": path.resolve(__dirname, "../../node_modules/test262-parser-tests/fail"),
 };
 
 let totalCount = 0;
@@ -26,15 +26,15 @@ let failedCount = 0;
 let passed = (name, file, log = false) => {
   passedCount++;
   if (log) {
-    console.log(chalk.green(`Test Suite "${name}", case ${file} passed ✅`));
+    process.stdout.write(chalk.green(`Test Suite "${name}", case ${file} passed ✅\n`));
   }
 };
 
 let failed = (e, name, file) => {
   failedCount++;
-  console.log(chalk.red(`Test Suite "${name}", case ${file} failed ❌`));
-  console.log(chalk.red(`${path.resolve(TESTS[name], file)}`));
-  console.log(chalk.red(`${e.message}`));
+  process.stdout.write(chalk.red(`Test Suite "${name}", case ${file} failed ❌\n`));
+  process.stdout.write(chalk.red(`${path.resolve(TESTS[name], file)}\n`));
+  process.stdout.write(chalk.red(`${e.message}\n`));
 };
 
 let runBatch = (name, isThrow) => {
@@ -55,17 +55,17 @@ let runBatch = (name, isThrow) => {
   });
 };
 
-runBatch('pass', false);
-runBatch('pass-explicit', false);
-runBatch('early', true);
-runBatch('fail', true);
+runBatch("pass", false);
+runBatch("pass-explicit", false);
+runBatch("early", true);
+runBatch("fail", true);
 
-console.log(chalk.green(`Total Cases: ${totalCount}`));
-console.log(chalk.green(`Passed Cases: ${passedCount}`));
-console.log(chalk.green(`ECMA-262 Coverage: ${(passedCount * 100 / totalCount).toFixed(2)}%`));
-console.log('');
-console.log(chalk.yellow(`Excluded Cases: ${excludedCount}`));
-console.log(chalk.red(`Failed Cases: ${failedCount}`));
+process.stdout.write(chalk.green(`Total Cases: ${totalCount}\n`));
+process.stdout.write(chalk.green(`Passed Cases: ${passedCount}\n`));
+process.stdout.write(chalk.green(`ECMA-262 Coverage: ${(passedCount * 100 / totalCount).toFixed(2)}%\n`));
+process.stdout.write("\n");
+process.stdout.write(chalk.yellow(`Excluded Cases: ${excludedCount}\n`));
+process.stdout.write(chalk.red(`Failed Cases: ${failedCount}\n`));
 
 if (failedCount > 0) {
   process.exit(1);
